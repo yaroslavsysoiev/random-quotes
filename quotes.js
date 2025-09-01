e82f0ce8617f861a746c59366f28bfbebcdb7c8a
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2025-09-01 13:58:16 +0300

    Adding additional module with quotes

---
+++ quotes.js
@@ -0,0 +1,61 @@
+const quotes = [
+  {
+    quote: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote: 'Innovation distinguishes between a leader and a follower.',
+    author: 'Steve Jobs',
+  },
+  {
+    quote:
+      "Your time is limited, so don't waste it living someone else's life.",
+    author: 'Steve Jobs',
+  },
+  {
+    quote: "Life is what happens when you're busy making other plans.",
+    author: 'John Lennon',
+  },
+  {
+    quote: 'Imagination is more important than knowledge.',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'Success usually comes to those who are too busy to be looking for it.',
+    author: 'Henry David Thoreau',
+  },
+  {
+    quote: 'In the middle of every difficulty lies opportunity.',
+    author: 'Albert Einstein',
+  },
+  {
+    quote:
+      'Do not go where the path may lead, go instead where there is no path and leave a trail.',
+    author: 'Ralph Waldo Emerson',
+  },
+  {
+    quote:
+      'When something is important enough, you do it even if the odds are not in your favor.',
+    author: 'Elon Musk',
+  },
+  {
+    quote:
+      'Happiness is not something ready made. It comes from your own actions.',
+    author: 'Dalai Lama',
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop.',
+    author: 'Confucius',
+  },
+  {
+    quote: 'The best way to predict the future is to invent it.',
+    author: 'Alan Kay',
+  },
+  {
+    quote: 'Whether you think you can or you think you can’t, you’re right.',
+    author: 'Henry Ford',
+  },
+];
+
+export default quotes;
